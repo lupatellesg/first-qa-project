8f33444b9b65d58e69f9090bb7c8e18f6db52993
lupatelles <gabriel.telles0000@gmail.com>
2026-01-27 18:52:47 -0300

    enviando validação CT1 e CT2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,24 @@
 //CT-01
-describe('Login com credenciais válidas', () => {
-  it('Acesso ao site Sauce Demo', () => {
-    cy.visit('https://example.cypress.io')
+describe('Testes Login', () => {
+  beforeEach(() => { cy.visit('https://www.saucedemo.com') })
+  it('Acesse o site Sauce Demo e faça Login com credenciais válidas', () => {
+    cy.get('[data-test="username"]')
+      .type('standard_user')
+    cy.get('[data-test="password"]')
+      .type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.url().should('include', '/inventory.html')
+  })
+
+  
+    //CT-02
+    it.only('Login com dados inválidos', () => {
+    cy.get('[data-test="username"]').type('random_user')
+    cy.get('[data-test="password"]').type('random_password')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="error"]').should('be.visible')
+    cy.url().should('not.include', '/inventory.html')
   })
 })
