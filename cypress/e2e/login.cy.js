7f2b8d4d0d975eb78850facb770e5e3bdd6f597b
lupatelles <gabriel.telles0000@gmail.com>
2026-01-27 17:12:25 -0300

    visit sauce

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
+//CT-01
+describe('Login com credenciais válidas', () => {
+  it('Acesso ao site Sauce Demo', () => {
     cy.visit('https://example.cypress.io')
   })
 })
