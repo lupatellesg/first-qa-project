364f5ffaf5a11c552e69774eaab1521e932fa11b
lupatelles <gabriel.telles0000@gmail.com>
2026-01-28 16:43:08 -0300

    commit CTs login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 //CT-01
-describe('Testes Login', () => {
+describe('Testes de Login', () => {
   beforeEach(() => { cy.visit('https://www.saucedemo.com') })
   it('Acesse o site Sauce Demo e faça Login com credenciais válidas', () => {
     cy.get('[data-test="username"]')
@@ -11,9 +11,9 @@ describe('Testes Login', () => {
     cy.url().should('include', '/inventory.html')
   })
 
-  
-    //CT-02
-    it.only('Login com dados inválidos', () => {
+
+  //CT-02
+  it('Faça Login com dados inválidos', () => {
     cy.get('[data-test="username"]').type('random_user')
     cy.get('[data-test="password"]').type('random_password')
     cy.get('[data-test="login-button"]').click()
@@ -21,4 +21,15 @@ describe('Testes Login', () => {
     cy.get('[data-test="error"]').should('be.visible')
     cy.url().should('not.include', '/inventory.html')
   })
+
+  //CT-06
+  it('Faça Login com usuário bloqueado', () => {
+    cy.get('[data-test="username"]').type('locked_out_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
+    cy.url().should('not.include', '/inventory.html')
+  })
+
 })
