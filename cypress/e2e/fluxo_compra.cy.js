8dc44c02042779ee3b1dfa578297c3176728ac85
lupatelles <gabriel.telles0000@gmail.com>
2026-01-30 18:10:32 -0300

    enviando CTs fluxo de compra e checkout

--- cypress/e2e/fluxo_compra.cy.js
+++ cypress/e2e/fluxo_compra.cy.js
@@ -1,5 +1,6 @@
 describe('Testes do fluxo de compra', () => {
-    beforeEach(() => { cy.visit('https://www.saucedemo.com')
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com')
         cy.get('[data-test="username"]').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('[data-test="login-button"]').click()
@@ -21,5 +22,28 @@ describe('Testes do fluxo de compra', () => {
         cy.url().should('eq', 'https://www.saucedemo.com/checkout-complete.html')
     })
 
-})
+    //CT-04
+    it('Checkout com campos obrigatórios vazios', () => {
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+        cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+        cy.get('[data-test="shopping-cart-link"]').click()
+        cy.get('[data-test="checkout"]').click()
+        cy.get('[data-test="continue"]').click()
 
+        cy.get('.error-message-container').should('have.text', 'Error: First Name is required')
+        cy.url().should('eq', 'https://www.saucedemo.com/checkout-step-one.html')
+    })
+
+
+    //CT-05
+    it.only('Remoção de item do carrinho', () => {
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('[data-test="shopping-cart-link"]').click()
+        cy.url().should('eq', 'https://www.saucedemo.com/cart.html')
+
+        cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+
+        cy.get('[data-test="inventory-item"]').should('not.exist')
+    })
+})
